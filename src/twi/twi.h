5de96b2af10a6cbb6ddf067e09a812524c7fa747
ComputerAided <ComputerAided@users.noreply.github.com>
2016-05-03 18:55:26 -0600

    Update twi.h

--- src/twi/twi.h
+++ src/twi/twi.h
@@ -2,9 +2,25 @@
 #ifndef ComputerAided_twi_h
 #define ComputerAided_twi_h
 
+void twiEnable();
+
+
 void twiStart();
+
+
+void twiStop();
+
+
 void twiSend();
-void twiEnd();
+
+
+uint8_t readACK();
+
+
+uint8_t readNoAck();
+
+
+void waitForTWIComplete();
 
 
 #endif
